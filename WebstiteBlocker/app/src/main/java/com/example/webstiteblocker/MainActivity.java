e991cb08cd7007d1f20a8e0fba949311ff406d8a
Empuran <jaya1999sankar@gmail.com>
2022-10-31 05:58:31 -0700

    commit

--- WebstiteBlocker/app/src/main/java/com/example/webstiteblocker/MainActivity.java
+++ WebstiteBlocker/app/src/main/java/com/example/webstiteblocker/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.webstiteblocker;
 
+import static android.app.Service.START_STICKY;
+
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.net.VpnService;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -25,6 +29,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @BindView(R.id.bt_add)
     ImageButton bt_add;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,10 +39,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bt_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
+               if(checkAccessibilityPermission()){
+                   Intent serviceIntent = new Intent(getApplicationContext(), UrlInterceptorService.class);
+                   startService(serviceIntent);
+               }
             }
         });
+    }
+
+    public boolean checkAccessibilityPermission () {
+        int accessEnabled = 0;
+        try {
+            accessEnabled = Settings.Secure.getInt(this.getContentResolver(), Settings.Secure.ACCESSIBILITY_ENABLED);
+        } catch (Settings.SettingNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (accessEnabled == 0) {
+
+            // if not construct intent to request permission
+            Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+            startActivity(intent);
+            return false;
+        } else {
+
+            return true;
+        }
     }
 
     @Override
